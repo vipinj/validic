5b190aacccaf5ae1d18c08313b2758e0fb9a86e8
Julius Francisco <baldrailers@gmail.com>
2014-01-25 06:39:52 +0800

    Adding organization_id and user_id attr_accessor

--- lib/validic.rb
+++ lib/validic.rb
@@ -8,7 +8,11 @@ directory = File.expand_path(File.dirname(__FILE__))
 module Validic
 
   class << self
-    attr_accessor :api_url, :api_version, :access_token
+    attr_accessor :api_url,
+      :api_version,
+      :access_token,
+      :organization_id,
+      :user_id
 
     ##
     # Configure default
@@ -23,9 +27,10 @@ module Validic
     private
 
     def load_defaults
-      self.api_url      ||= 'https://api.validic.com'
-      self.api_version  ||= 'v1'
-      self.access_token ||= 'DEMO_KEY'
+      self.api_url          ||= 'https://api.validic.com'
+      self.api_version      ||= 'v1'
+      self.access_token     ||= 'ENTERPRISE_KEY'
+      self.organization_id  ||= '51aca5a06dedda916400002b'
     end
   end
 
--- lib/validic/client.rb
+++ lib/validic/client.rb
@@ -2,7 +2,6 @@
 require 'forwardable'
 require 'validic/request'
 require 'validic/organization'
-require 'validic/activity'
 require 'validic/user'
 require 'validic/profile'
 require 'validic/fitness'
@@ -21,7 +20,6 @@ module Validic
 
     include Request
     include Organization
-    include Activity
     include User
     include Profile
     include Fitness
@@ -34,7 +32,10 @@ module Validic
     include TobaccoCessation
     include ThirdPartyApp
 
-    attr_reader :api_url, :api_version, :access_token
+    attr_reader :api_url,
+      :api_version,
+      :access_token,
+      :organization_id
 
     ##
     # Create a new Validic::Client object
@@ -44,6 +45,7 @@ module Validic
       @api_url        = options[:api_url]       || Validic.api_url
       @api_version    = options[:api_version]   || Validic.api_version || 'v1'
       @access_token   = options[:access_token]  || Validic.access_token
+      @organization_id = options[:organization_id] || Validic.organization_id
     end
 
     ##
@@ -52,7 +54,7 @@ module Validic
     # @return [Faraday::Connection]
     def connection
       params = {}
-      @connection = Faraday.new(url: api_url, params: params, headers: default_headers, ssl: {verify: false}) do |faraday|
+      @connection = Faraday.new(url: api_url, params: params, headers: default_headers, ssl: {verify: true}) do |faraday|
         faraday.use FaradayMiddleware::Mashify
         faraday.use FaradayMiddleware::ParseJson, content_type: /\bjson$/
         faraday.use FaradayMiddleware::FollowRedirects
@@ -60,10 +62,49 @@ module Validic
       end
     end
 
+    ##
+    # Pull the latest endpoint
+    def latest(type, org_id=nil, user_id=nil, params={})
+      organization_id = org_id.nil? ? Validic.organization_id : org_id
+      user_id = org_id.nil? ? Validic.user_id : org_id
+
+      url = "/#{Validic.api_version}/organizations/#{organization_id}/#{type.to_s}/latest.json"
+
+      if user_id
+        url = "/#{Validic.api_version}/organizations/#{organization_id}/users/#{user_id}/#{type.to_s}/latest.json"
+      end
+
+      get(url, params)
+    end
+
+    ##
+    # Generic Pull of Validic Objects
+    def get_endpoint(type, params={})
+
+      url = "/#{Validic.api_version}/organizations/#{Validic.organization_id}/#{type.to_s}.json"
+
+      if Validic.user_id
+        url = "/#{Validic.api_version}/organizations/#{Validic.organization_id}/users/#{Validic.user_id}/#{type.to_s}.json"
+      end
+
+      get(url, params)
+    end
+
+    ##
+    # Generic POST to Validic
+    def post_to_validic(type, params={})
+      url = "/#{Validic.api_version}/organizations/#{Validic.organization_id}/#{type.to_s}.json"
+
+      if Validic.user_id
+        url = "/#{Validic.api_version}/organizations/#{Validic.organization_id}/users/#{Validic.user_id}/#{type.to_s}.json"
+      end
+      post(url, params)
+    end
+
     private
 
     def default_headers
-      headers = {
+      {
         accept: 'application/json',
         content_type: 'application/json',
         user_agent: "Ruby Gem by Validic #{Validic::VERSION}"
