5a4fb1bb484898ccf6da0408948f38798345bad9
Anthony Ross <anthony.s.ross@gmail.com>
2015-01-13 09:56:23 -0500

    Update to sleep and bulk_sleep

--- spec/validic/rest/sleep_spec.rb
+++ spec/validic/rest/sleep_spec.rb
@@ -28,7 +28,7 @@ describe Validic::REST::Sleep do
       before do
         stub_get("/organizations/#{ENV['TEST_ORG_ID']}/users/#{ENV['TEST_USER_ID']}/sleep.json").
           with(query: { access_token: ENV['TEST_ORG_TOKEN'] }).
-          to_return(body: fixture('sleep.json'), query: { access_token: ENV['TEST_ORG_TOKEN'] }, headers: {content_type: 'application/json; charset=utf-8'})
+          to_return(body: fixture('bulk_sleep.json'), query: { access_token: ENV['TEST_ORG_TOKEN'] }, headers: {content_type: 'application/json; charset=utf-8'})
         @sleep = client.get_sleep(user_id: ENV['TEST_USER_ID'])
       end
 
@@ -44,75 +44,3 @@ describe Validic::REST::Sleep do
 
   end
 end
-
-# context "#get_sleeps by organization" do
-#   before do
-#     @sleep = client.get_sleeps({organization_id: "51aca5a06dedda916400002b", access_token: "ENTERPRISE_KEY"})
-#   end
-#
-#   it "returns JSON response of Validic::Sleep", vcr: true do
-#     expect(@sleep).not_to be_nil
-#   end
-#
-#   it "status 200" do
-#     expect(@sleep.summary.status).to eq(200)
-#   end
-#
-#   it "has summary node" do
-#     expect(@sleep.summary).not_to be_nil
-#   end
-# end
-#
-# context "#get_sleeps by user" do
-#   before do
-#     @sleep = client.get_sleeps({user_id: ENV['TEST_USER_ID']})
-#   end
-#
-#   it "returns JSON response of Validic::Sleep", vcr: true do
-#     expect(@sleep).not_to be_nil
-#   end
-#
-#   it "status 200" do
-#     expect(@sleep.summary.status).to eq(200)
-#   end
-#
-#   it "has summary node" do
-#     expect(@sleep.summary).not_to be_nil
-#   end
-# end
-#
-# context "Validic Connect" do
-#   before do
-#     @connect = Validic::Client.new(organization_id: ENV['PARTNER_ORG_ID'],
-#                                    access_token: ENV['PARTNER_ACCESS_TOKEN'],
-#                                    api_url: 'https://api.validic.com',
-#                                    api_version: 'v1')
-#   end
-#
-#   context "#create_sleep" do
-#     it "should create new sleep record", vcr: true do
-#       @new_sleep = @connect.create_sleep(ENV['PARTNER_USER_ID'], "sleepz")
-#       expect(@new_sleep).not_to be_nil
-#       expect(@new_sleep.sleep.activity_id).to eq "sleepz"
-#       expect(@new_sleep.sleep.source).to eq "healthy_yet"
-#     end
-#   end
-#
-#   context "#update_sleep" do
-#     it "should update sleep record", vcr: true do
-#       @update_sleep = @connect.update_sleep(ENV['PARTNER_USER_ID'], "54aeef3984626b1806000246", awake: 10.0)
-#       expect(@update_sleep).not_to be_nil
-#       expect(@update_sleep.sleep.awake).to eq 10.0
-#       expect(@update_sleep.sleep.source).to eq "healthy_yet"
-#     end
-#   end
-#
-#   context "#delete_sleep" do
-#     it "should delete sleep record", vcr: true do
-#       @delete_sleep = @connect.delete_sleep(ENV['PARTNER_USER_ID'], "54aeef3984626b1806000246")
-#       expect(@delete_sleep.code).to eq 200
-#     end
-#   end
-# end
-#
-# end
