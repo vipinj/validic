e62cdb15ab9aaff6c8a668305e37a9cb8ef8b53e
Anthony Ross <anthony.s.ross@gmail.com>
2015-01-15 14:46:56 -0500

    Added extra class

--- lib/validic/nutrition.rb
+++ lib/validic/nutrition.rb
@@ -1,9 +1,16 @@
+require 'validic/extra'
 module Validic
   class Nutrition
     def initialize(attrs = {})
       attrs.each do |k, v|
-        instance_variable_set("@#{k}", v)
-        self.class.send(:attr_reader, k)
+        if k == "extras"
+          v = Validic::Extra.new(JSON.parse(v))
+          instance_variable_set("@#{k}", v)
+          self.class.send(:attr_reader, k)
+        else
+          instance_variable_set("@#{k}", v)
+          self.class.send(:attr_reader, k)
+        end
       end
     end
   end
