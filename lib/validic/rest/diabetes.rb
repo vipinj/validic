b77108db6a420ccf384eebc3deaf7ba948613a08
Josh Langholtz <josh.langholtz@validic.com>
2015-01-16 12:52:58 -0500

    Normalize diabetes methods.

--- lib/validic/rest/diabetes.rb
+++ lib/validic/rest/diabetes.rb
@@ -9,20 +9,23 @@ module Validic
         build_response_attr(resp)
       end
 
-      def create_diabetes(user_id, options = {})
+      def create_diabetes(options = {})
+        user_id = options.delete(:user_id)
         options = { user_id: user_id, diabetes: options }
         response = post_request(:diabetes, options)
         Validic::Diabetes.new(response['diabetes'])
       end
 
-      def update_diabetes(user_id, activity_id, options = {})
-        options = { user_id: user_id, activity_id: activity_id, diabetes: options }
+      def update_diabetes(options = {})
+        user_id, _id = options.delete(:user_id), options.delete(:_id)
+        options = { user_id: user_id, _id: _id, diabetes: options }
         response = put_request(:diabetes, options)
         Validic::Diabetes.new(response['diabetes'])
       end
 
-      def delete_diabetes(user_id, activity_id)
-        options = { user_id: user_id, activity_id: activity_id }
+      def delete_diabetes(options = {})
+        user_id, _id = options.delete(:user_id), options.delete(:_id)
+        options = { user_id: user_id, _id: _id }
         delete_request(:diabetes, options)
         true
       end
