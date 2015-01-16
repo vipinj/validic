b52933cc635abf8d63ba4ea3edd0899c62e3c210
Anthony Ross <anthony.s.ross@gmail.com>
2015-01-16 15:27:47 -0500

    Update to include alias from vers 0.3X

--- lib/validic/rest/users.rb
+++ lib/validic/rest/users.rb
@@ -24,27 +24,32 @@ module Validic
         response = post_request(:users, { user: options })
         Validic::User.new(response['user'])
       end
+      alias :user_provision :provision_user
 
       def update_user(options = {})
         user_id = options.delete(:user_id)
         response = put_request(:users, { user_id: user_id, user: options })
         Validic::User.new(response['user'])
       end
+      alias :user_update :update_user
 
       def delete_user(options = {})
         delete_request(:users, { user_id: options[:user_id] })
         true
       end
+      alias :user_delete :delete_user
 
       def suspend_user(options = {})
         put_request(:users, { user_id: options[:user_id], suspend: '1' })
         true
       end
+      alias :user_suspend :suspend_user
 
       def unsuspend_user(options = {})
         put_request(:users, { user_id: options[:user_id], suspend: '0' })
         true
       end
+      alias :user_unsuspend :unsuspend_user
     end
   end
 end
