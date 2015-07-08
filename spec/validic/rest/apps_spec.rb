14f0bd49b6fb9891c43696495c01ce051c045d6c
Anthony Ross <anthony.ross@validic.com>
2015-07-08 06:06:04 -0400

    APIOLD-594 #time 30m
    This change was brought up in issue #20 :
    https://github.com/Validic/validic/issues/20
    
    Previously the wrapper did not support the expanded filter that we
    outline in our docs, enabling customers to get extra fields for certain
    applications.

--- spec/validic/rest/apps_spec.rb
+++ spec/validic/rest/apps_spec.rb
@@ -4,19 +4,38 @@ describe Validic::REST::Apps do
   let(:client) { Validic::Client.new }
 
   describe "#get_apps" do
-    before do
-      stub_get("/organizations/1/apps.json")
-        .with(query: { access_token: '1' })
-        .to_return(body: fixture('apps.json'),
-      headers: { content_type: 'application/json; charset=utf-8' })
-        @apps = client.get_org_apps
+    context 'non-expanded' do
+      before do
+        stub_get("/organizations/1/apps.json")
+          .with(query: { access_token: '1' })
+          .to_return(body: fixture('apps.json'),
+        headers: { content_type: 'application/json; charset=utf-8' })
+          @apps = client.get_org_apps
+      end
+
+      it 'returns a response object' do
+        expect(@apps).to be_a Validic::Response
+      end
+      it 'makes an apps request to the correct url' do
+        expect(a_get('/organizations/1/apps.json')
+          .with(query: { access_token: '1' })).to have_been_made
+      end
     end
-    it 'returns a response object' do
-      expect(@apps).to be_a Validic::Response
-    end
-    it 'makes an apps request to the correct url' do
-      expect(a_get('/organizations/1/apps.json')
-        .with(query: { access_token: '1' })).to have_been_made
+    context 'expanded' do
+      before do
+        stub_get("/organizations/1/apps.json")
+          .with(query: { access_token: '1', expanded: 1 })
+          .to_return(body: fixture('apps.json'),
+        headers: { content_type: 'application/json; charset=utf-8' })
+          @apps = client.get_org_apps(expanded: 1)
+      end
+      it 'returns a response object' do
+        expect(@apps).to be_a Validic::Response
+      end
+      it 'makes an apps request to the correct url' do
+        expect(a_get('/organizations/1/apps.json')
+          .with(query: { access_token: '1', expanded: '1' })).to have_been_made
+      end
     end
   end
 
@@ -26,7 +45,7 @@ describe Validic::REST::Apps do
         .with(query: { authentication_token: '2', access_token: '1' })
         .to_return(body: fixture('synced_apps.json'),
       headers: { content_type: 'application/json; charset=utf-8' })
-      @synced_apps = client.get_user_synced_apps(authentication_token: '2')
+        @synced_apps = client.get_user_synced_apps(authentication_token: '2')
     end
     it 'returns a response object' do
       expect(@synced_apps).to be_a Validic::Response
